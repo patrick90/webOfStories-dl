40541c0f9a83480b144f01488d05b379b13009a0
patrick90 <patrykszweda90@gmail.com>
2014-04-24 12:36:12 +0200

    All working

---
+++ README.txt
@@ -0,0 +1,18 @@
+HOW TO USE:
+First,  make sure you have BeautifulSoup installed.
+
+You can install it with
+
+pip install beautifulsoup4
+
+or
+
+easy_install beautifulsoup4.
+
+For more info, see http://www.crummy.com/software/BeautifulSoup/#Download.
+
+Two, how to use.
+Let say you want to downloand transcript of all stories of Marvin Minsky from webofstories, then you should write this in console:
+python webos.py http://www.webofstories.com/play/marvin.minsky/
+
+In this same folder that you run this script there will be created a html file, with all transcripts. In this example: marvin.minsky.html
---
+++ wosDl.py
@@ -0,0 +1,45 @@
+﻿__author__ = 'hl'
+from bs4 import BeautifulSoup
+from urllib import urlopen
+import re
+import sys
+
+def howManySubpages(masterWebpage):
+    r=re.compile("activatePaginationLinks\(true, \d*, \d*\)")
+    page= urlopen(masterWebpage).read()
+    sa=re.findall(r, page)[0].split(',')[2]
+    sa=sa[1:-1]
+    return int(sa)
+
+def extractTranscript(url):
+    soup=BeautifulSoup(urlopen(url))
+    transcript=str(soup.find(id="transcript-en"))
+    title = str(soup.find("title"))
+    title=title.replace("title", "h2")
+    return title+transcript
+
+def extractAllText(url):
+    text=""
+    title=url.split("/")[-2]
+    myFile=codecs.open(title+".html", 'w')
+    n=howManySubpages(url+"1")+1
+    myFile.write('<head> <meta http-equiv="Content-Type" content="application/xhtml+xml; charset=utf-8" /> </head>')
+    for i in range(1, n):
+        myFile.write(extractTranscript(url+str(i)) )
+        print("Percent " + str(float(i)/n))
+    myFile.close()
+
+
+def main():
+    url = sys.argv[1]
+    extractAllText(url)
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\n\nCTRL-C detected, shutting down....")
+        sys.exit(0)
+    except IndexError:
+        print("You didn't specified url")
+        sys.exit(0)
